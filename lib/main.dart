b07b4955b1279d1aa433f50b0893e90f4ea0f0fc
Bonish_Singh <bonishsinghbrar@gmail.com>
2024-04-19 09:34:43 -0400

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,67 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false, // Remove debug banner
-      home: PokemonList(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pokemon Cards'),
+        ),
+        body: PokemonList(), // Add PokemonList back to the body
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Navigation Menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Home'),
+                onTap: () {
+                  Navigator.pop(context); // Close the drawer
+                  // You can add any logic here for navigating to the home screen if needed
+                },
+              ),
+              ListTile(
+                title: Text('Logout'), // Changed Login to Logout
+                onTap: () {
+                  Navigator.pop(context); // Close the drawer
+                  showLogoutDialog(context); // Show logout dialog
+                },
+              ),
+              // Add more list tiles for other menu items if necessary
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Logout"),
+          content: Text("User successfully logged out"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => PokemonList())); // Navigate back to PokemonList
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -69,6 +129,7 @@ class PokemonList extends StatefulWidget {
 
 class _PokemonListState extends State<PokemonList> {
   List<dynamic> pokemonData = [];
+  bool isLoggedIn = false; // Track if user is logged in
 
   @override
   void initState() {
@@ -89,7 +150,8 @@ class _PokemonListState extends State<PokemonList> {
     }
   }
 
-  void showPaymentSuccessfulDialog(String itemName, String itemImage, double marketPrice) {
+  void showPaymentSuccessfulDialog(
+      String itemName, String itemImage, double marketPrice) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -104,6 +166,7 @@ class _PokemonListState extends State<PokemonList> {
               Image.network(itemImage),
               SizedBox(height: 8),
               Text("Market Price: \$${marketPrice.toStringAsFixed(2)}"),
+              if (isLoggedIn) Text("User: John Doe"), // Display user name if logged in
             ],
           ),
           actions: [
@@ -119,34 +182,87 @@ class _PokemonListState extends State<PokemonList> {
     );
   }
 
+  void handleBuyNow(String itemName, String itemImage, double marketPrice) {
+    if (isLoggedIn) {
+      showPaymentSuccessfulDialog(itemName, itemImage, marketPrice);
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+          .then((isLoggedIn) {
+        if (isLoggedIn != null && isLoggedIn) {
+          showPaymentSuccessfulDialog(itemName, itemImage, marketPrice);
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: pokemonData.length,
+      itemBuilder: (BuildContext context, int index) {
+        final pokemon = pokemonData[index];
+        final marketPrice =
+        pokemon['tcgplayer']['prices']['holofoil']['market'];
+        return ListTile(
+          leading: Image.network(pokemon['images']['small']),
+          title: Text(pokemon['name']),
+          subtitle: Text('Market Price: \$${marketPrice.toStringAsFixed(2)}'),
+          trailing: ElevatedButton(
+            onPressed: () {
+              handleBuyNow(
+                pokemon['name'],
+                pokemon['images']['small'],
+                marketPrice,
+              );
+            },
+            child: Text("Buy Now"),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pokemon Cards'),
+        title: Text('Customer Detail'),
       ),
-      body: ListView.builder(
-        itemCount: pokemonData.length,
-        itemBuilder: (BuildContext context, int index) {
-          final pokemon = pokemonData[index];
-          final marketPrice =
-          pokemon['tcgplayer']['prices']['holofoil']['market'];
-          return ListTile(
-            leading: Image.network(pokemon['images']['small']),
-            title: Text(pokemon['name']),
-            subtitle: Text('Market Price: \$${marketPrice.toStringAsFixed(2)}'),
-            trailing: ElevatedButton(
-              onPressed: () {
-                showPaymentSuccessfulDialog(
-                  pokemon['name'],
-                  pokemon['images']['small'],
-                  marketPrice,
-                );
-              },
-              child: Text("Buy Now"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'First Name'),
             ),
-          );
-        },
+            TextField(
+              decoration: InputDecoration(labelText: 'Last Name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Card Number'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'CVV'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Expiry Date'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Add login logic here
+                // For demo purpose, just set isLoggedIn to true
+                Navigator.pop(context, true); // Close the login screen and return isLoggedIn
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('User Detail saved successfully!')));
+              },
+              child: Text('Save Details'),
+            ),
+          ],
+        ),
       ),
     );
   }
